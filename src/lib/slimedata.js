af9af9999cbc3822b02787f8e94dd75ed4465046
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-07 15:11:12 -0400

    Add docile slimes

---
+++ src/lib/slimedata.js
@@ -0,0 +1,39 @@
+const docileSlimes = [
+    {name: "Pink Slime", palette: ["#de4367", "#f8738e", "#d62f45"]},
+    {name: "Tabby Slime", palette: ["#545048", "#6c695f", "#a39986", "#352f1c"]},
+    {name: "Phosphor Slime", palette: ["#5f5fb3", "#7e7ec2", "#bdbae3", "#b7b9a0", "#f5fc82"]},
+    {name: "Honey Slime", palette: ["#2d1702", "#7c3a13", "#b85e03", "#a06300", "#b57c01"]},
+    {name: "Puddle Slime", palette: ["#095d96", "#0a607e", "#29c6d6", "#0c78ab"]},
+    {name: "Hunter Slime", palette: ["#201a30", "#9a717d", "#2f2342", "#633933", "#dcca2a"]},
+    {name: "Quantum Slime", palette: ["#753915", "#a65c19", "#ba8122", "#dfc142", "#fff55b"]},
+    {name: "Dervish Slime", palette: ["#4e4b6f", "#928cac", "#764c8b", "#41234f", "#85679a"]},
+    {name: "Tangle Slime", palette: ["#186251", "#31a490", "#a2e0c2", "#43581b", "#c7b237"]},
+    {name: "Saber Slime", palette: ["#825d3c", "#b88957", "#ecbf83", "#451b07", "#b89d7d"]},
+];
+
+const harmfulSlimes = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+];
+
+const specialSlimes = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+];
+
+const hostileSlimes = [
+    {},
+    {},
+];
+
+export { docileSlimes, harmfulSlimes, specialSlimes, hostileSlimes };
