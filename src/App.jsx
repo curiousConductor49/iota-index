69b0407c92e26a45c4fe38afe083258184814c67
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-13 16:13:55 -0400

    Comment out import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router'
-import Navbar from './components/Navbar.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Categories from './pages/Categories.jsx'
-import Docile from './pages/Docile.jsx'
-import Harmful from './pages/Harmful.jsx'
-import Hostile from './pages/Hostile.jsx'
-import Special from './pages/Special.jsx'
+// import { Routes, Route } from 'react-router'
+// import Navbar from './components/Navbar.jsx'
+// import Home from './pages/Home.jsx'
+// import About from './pages/About.jsx'
+// import Categories from './pages/Categories.jsx'
+// import Docile from './pages/Docile.jsx'
+// import Harmful from './pages/Harmful.jsx'
+// import Hostile from './pages/Hostile.jsx'
+// import Special from './pages/Special.jsx'
 
 function App() {
   return (
