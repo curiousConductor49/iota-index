ad91bb21a82cd098425578468a981a2c534a2211
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-13 13:59:57 -0400

    Add browser router component

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +8,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
   </StrictMode>,
 )
