22bee6187c80b17795f00bf7bf0aeda546df501d
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-13 18:08:13 -0400

    Wrap return statement content in fragments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+        </>
+        
     );
 }
 
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,9 @@
 function NotFound() {
     return(
-        <h1>Page Not Found</h1>
+        <>
+            <h1>Page Not Found</h1>
+        </>
+        
     );
 }
 
