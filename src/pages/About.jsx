32d1e08e064915bbacc72005ee7158b5021794e2
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-14 17:09:43 -0400

    Build about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 function About() {
     return (
         <>
-            <h1>About</h1>
+            <h1 className="text-center font-sans text-5xl m-10">About</h1>
+            <div className="max-w-2xl m-auto flex flex-col gap-10">
+                <p className="text-center font-sans text-base">Slime Rancher was a special part of my childhood growing up. This colour palette index is a little thank you for all the squishy, silly memories I made watching Let's Plays of this game, a world where the scientific colour of cheer was everywhere and the sun always rose on a new adventure in the Far, Far Range.</p>
+                <p className="text-center font-sans text-base">I hope you enjoy it as much I had fun making it!</p>
+                <p className="text-center font-sans text-base">Special notice: [Project Title] is unofficial Fan Content permitted under <a href="https://www.slimerancher.com/fan-content-policy/">the Fan Content Policy</a>. Not approved/endorsed by Monomi Park. Portions of the materials used are property of Monomi Park, LLC. ©Monomi Park LLC.</p>
+            </div>
         </>
     );
 }
