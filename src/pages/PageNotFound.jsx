8da8f167752c18f855ba62982bb8b99991ee104e
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-14 16:20:10 -0400

    Build page for when the user navigates to a non-existent url

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router'
+
 function NotFound() {
     return(
         <>
-            <h1>Page Not Found</h1>
+            <h1 className="text-center font-sans text-5xl m-10">Page Not Found</h1>
+            <p className="text-center font-sans text-lg">Uh oh! Looks like you've landed in the middle of nowhere on the Far, Far Range!</p>
         </>
-        
     );
 }
 
