5c10ae255e47a0f230d1bb95edec7549c042a219
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-14 16:56:09 -0400

    Build home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <h1 className="text-center font-sans text-5xl m-10">[Project Title]</h1>
+            <h2 className="text-center font-sans text-3xl"> A Slime Rancher Fan Project</h2>
+            <section className="flex flex-col m-5">
+                <p className="text-center font-sans text-lg">Howdy, rancher! Have you ever wanted to know all the wonderful colours of your wonderful slimes?</p>
+                <p className="text-center font-sans text-lg">Well, look no further! Here, we've got slimes dime a dozen, categorized by behaviour and detailed by colour palettes!</p>
+            </section>
         </>
         
     );
