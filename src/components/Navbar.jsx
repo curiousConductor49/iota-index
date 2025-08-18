538e3b8e6749189066fe47f46b69a76ef7fd400c
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-18 13:09:39 -0400

    Build out navbar and replace link with navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 
 function Navbar() {
     return (
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/categories">Categories</Link></li>
+        <ul className="border-b-3 border-red-400 font-sans text-2xl flex items-center gap-10 p-5">
+            <li className="mr-auto"><NavLink to="/"><img src="src/assets/images/docile/Pink_Slime.png" className="h-[60px] w-[60px]"/></NavLink></li>
+            <li><NavLink to="/" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Home</NavLink></li>
+            <li><NavLink to="/categories" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Categories</NavLink></li>
+            <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>About</NavLink></li>
         </ul>
     );
 }
