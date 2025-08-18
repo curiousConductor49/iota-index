12df51acb1e2eabd796df3453e2afcae7a7a507d
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-18 13:43:51 -0400

    Update notfound page to use useNavigate hook

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+
 function NotFound() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/")
+        }, 4000)
+    }, [])
+
     return(
         <>
             <h1 className="text-center font-sans text-5xl m-10">Page Not Found</h1>
             <p className="text-center font-sans text-lg">Uh oh! Looks like you've landed in the middle of nowhere on the Far, Far Range!</p>
+            <p className="text-center font-sans text-lg m-10">Let's get you back home, huh?</p>
         </>
     );
 }
