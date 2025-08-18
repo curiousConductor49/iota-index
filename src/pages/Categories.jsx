f7da45edfade7550e0dda96a733fdc2d9cad1065
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-18 11:40:31 -0400

    Build categories page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router'
 function Categories() {
     return (
         <>
-            <h1>Categories</h1>
-            <ul>
+            <h1 className="text-center font-sans text-5xl m-10">Categories</h1>
+            <p className="text-center font-sans text-xl mb-10">Slimes are classed as one of five behaviour types.</p>
+            <ul className="text-center font-sans text-2xl flex flex-col gap-5">
                 <li><Link to="/categories/docile">Docile</Link></li>
                 <li><Link to="/categories/harmful">Harmful</Link></li>
                 <li><Link to="/categories/hostile">Hostile</Link></li>
