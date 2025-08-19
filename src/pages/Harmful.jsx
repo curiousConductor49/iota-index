09906841fe72fe804b94d518e02ccaa9ee5aa720
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 14:49:24 -0400

    Import imgs and slimecard component

--- src/pages/Harmful.jsx
+++ src/pages/Harmful.jsx
@@ -1,9 +1,20 @@
+import Slimecard from "../components/Slimecard";
 import { harmfulSlimes } from "../lib/slimedata";
+import rock from '../assets/images/harmful/Rock_Slime.png'
+import rad from '../assets/images/harmful/Rad_Slime.png'
+import boom from '../assets/images/harmful/Boom_Slime.png'
+import crystal from '../assets/images/harmful/Crystal_Slime.png'
+import fire from '../assets/images/harmful/Fire_Slime.png'
+import mosaic from '../assets/images/harmful/Mosaic_Slime.png'
 
 function Harmful() {
+    const harmfulImgs = [rock, rad, boom, crystal, fire, mosaic];
     return (
         <>
             <h1>Harmful Slimes</h1>
+            {harmfulImgs.map((img, index) => {
+                return <Slimecard name={harmfulSlimes[index].name} palette={harmfulSlimes[index].palette} src={img} key={index}></Slimecard>
+            })}
         </>
     );
 }
