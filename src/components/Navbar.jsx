88bbfa31ec350fd4d74a927b035c589d208605a5
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 13:24:33 -0400

    Update navbar img path to use import statement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router'
+import pink from '../assets/images/docile/Pink_Slime.png'
 
 function Navbar() {
     return (
         <ul className="border-b-3 border-red-400 font-sans text-2xl flex items-center gap-10 p-5">
-            <li className="mr-auto"><NavLink to="/"><img src="src/assets/images/docile/Pink_Slime.png" className="h-[60px] w-[60px]"/></NavLink></li>
+            <li className="mr-auto"><NavLink to="/"><img src={pink} className="h-[60px] w-[60px]"/></NavLink></li>
             <li><NavLink to="/" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Home</NavLink></li>
             <li><NavLink to="/categories" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Categories</NavLink></li>
             <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>About</NavLink></li>
