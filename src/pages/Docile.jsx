ee5ec6a3ae39a649f3d828e8acbfcc03e198ca2b
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 13:25:27 -0400

    Test usage of slimecard

--- src/pages/Docile.jsx
+++ src/pages/Docile.jsx
@@ -1,9 +1,12 @@
+import Slimecard from "../components/Slimecard";
 import { docileSlimes } from "../lib/slimedata";
+import pink from '../assets/images/docile/Pink_Slime.png'
 
 function Docile() {
     return (
         <>
             <h1>Docile Slimes</h1>
+            <Slimecard name={docileSlimes[0].name} palette={docileSlimes[0].palette} src={pink}></Slimecard>
         </>
     );
 }
