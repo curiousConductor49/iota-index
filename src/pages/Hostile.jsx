71858995fbb97e860faf61e79e5cffbca5e336cb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 14:51:39 -0400

    Import imgs and slimecard component

--- src/pages/Hostile.jsx
+++ src/pages/Hostile.jsx
@@ -1,9 +1,16 @@
+import Slimecard from "../components/Slimecard";
 import { hostileSlimes } from "../lib/slimedata";
+import feral from '../assets/images/hostile/Feral_Slime.png'
+import tarr from '../assets/images/hostile/Tarr_Slime.png'
 
 function Hostile() {
+    const hostileImgs = [feral, tarr];
     return (
         <> 
             <h1>Hostile Slimes</h1>
+            {hostileImgs.map((img, index) => {
+                return <Slimecard name={hostileSlimes[index].name} palette={hostileSlimes[index].palette} src={img} key={index}></Slimecard>
+            })}
         </>
     );
 }
