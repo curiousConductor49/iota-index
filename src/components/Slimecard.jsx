04152ec63b18d2485f261fa9ec9de680e793a717
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 17:10:57 -0400

    Comment out palette div (temporary)

--- src/components/Slimecard.jsx
+++ src/components/Slimecard.jsx
@@ -2,7 +2,9 @@ function Slimecard(props) {
     return (
         <div>
             <p>Name: {props.name}</p>
-            <p>Palette: {props.palette}</p>
+            {/* <div>Palette: {props.palette.map((colour, index) => {
+                return <div className="bg-docile-pink-1 w-[50px] h-[150px]" key={index}></div>
+            })}</div> */}
             <img src={props.src} className="h-[150px] w-[150px]"/>
         </div>
     );
