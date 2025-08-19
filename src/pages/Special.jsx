e48470db42d3f1c0e81c922d0dfdfaaf04b12b96
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-19 14:54:47 -0400

    Import imgs and slimecard component

--- src/pages/Special.jsx
+++ src/pages/Special.jsx
@@ -1,9 +1,19 @@
+import Slimecard from "../components/Slimecard";
 import { specialSlimes } from "../lib/slimedata";
+import quicksilver from "../assets/images/special/Quicksilver_Slime.png"
+import glitch from "../assets/images/special/Glitch_Slime.png"
+import gold from "../assets/images/special/Gold_Slime.png"
+import lucky from "../assets/images/special/Lucky_Slime.png"
+import twinkle from "../assets/images/special/Twinkle_Slime.png"
 
 function Special() {
+    const specialImgs = [quicksilver, glitch, gold, lucky, twinkle];
     return (
         <>
             <h1>Special Slimes</h1>
+            {specialImgs.map((img, index) => {
+                return <Slimecard name={specialSlimes[index].name} palette={specialSlimes[index].palette} src={img} key={index}></Slimecard>
+            })}
         </>
     );
 }
