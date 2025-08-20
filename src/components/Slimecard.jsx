752c5de0ad54d81f42703cfb35e781195dab9da0
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-20 17:13:34 -0400

    Update card appearance and logic to pull colours from slimedata file

--- src/components/Slimecard.jsx
+++ src/components/Slimecard.jsx
@@ -1,10 +1,12 @@
+import { docileSlimes } from "../lib/slimedata";
+
 function Slimecard(props) {
     return (
         <div>
             <p>Name: {props.name}</p>
-            {/* <div>Palette: {props.palette.map((colour, index) => {
-                return <div className="bg-docile-pink-1 w-[50px] h-[150px]" key={index}></div>
-            })}</div> */}
+            <div className="flex flex-row">Palette: {docileSlimes[props.slimeNum].palette.map((colour, index) => {
+                return <div className={`bg-${colour} w-[50px] h-[150px]`} key={index}></div>
+            })}</div>
             <img src={props.src} className="h-[150px] w-[150px]"/>
         </div>
     );
