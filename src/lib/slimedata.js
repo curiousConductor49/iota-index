14507fd6367d6b6ecc30eb5b43f782a2ca26c69c
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-20 19:02:59 -0400

    Reformat data into a single object export

--- src/lib/slimedata.js
+++ src/lib/slimedata.js
@@ -1,36 +1,35 @@
-const docileSlimes = [
-    {name: "Pink Slime", palette: ["#de4367", "#f8738e", "#d62f45"]},
-    {name: "Tabby Slime", palette: ["#545048", "#6c695f", "#a39986", "#352f1c"]},
-    {name: "Phosphor Slime", palette: ["#5f5fb3", "#7e7ec2", "#bdbae3", "#b7b9a0", "#f5fc82"]},
-    {name: "Honey Slime", palette: ["#2d1702", "#7c3a13", "#b85e03", "#a06300", "#b57c01"]},
-    {name: "Puddle Slime", palette: ["#095d96", "#0a607e", "#0c78ab", "#29c6d6"]},
-    {name: "Hunter Slime", palette: ["#201a30", "#9a717d", "#2f2342", "#633933", "#dcca2a"]},
-    {name: "Quantum Slime", palette: ["#753915", "#a65c19", "#ba8122", "#dfc142", "#fff55b"]},
-    {name: "Dervish Slime", palette: ["#4e4b6f", "#928cac", "#764c8b", "#41234f", "#85679a"]},
-    {name: "Tangle Slime", palette: ["#186251", "#31a490", "#a2e0c2", "#43581b", "#c7b237"]},
-    {name: "Saber Slime", palette: ["#825d3c", "#b88957", "#ecbf83", "#451b07", "#b89d7d"]},
-];
+const slimeIndex = {
+    docileSlimes: [
+        {name: "Pink Slime", palette: ["#de4367", "#f8738e", "#d62f45"]},
+        {name: "Tabby Slime", palette: ["#545048", "#6c695f", "#a39986", "#352f1c"]},
+        {name: "Phosphor Slime", palette: ["#5f5fb3", "#7e7ec2", "#bdbae3", "#b7b9a0", "#f5fc82"]},
+        {name: "Honey Slime", palette: ["#2d1702", "#7c3a13", "#b85e03", "#a06300", "#b57c01"]},
+        {name: "Puddle Slime", palette: ["#095d96", "#0a607e", "#0c78ab", "#29c6d6"]},
+        {name: "Hunter Slime", palette: ["#201a30", "#9a717d", "#2f2342", "#633933", "#dcca2a"]},
+        {name: "Quantum Slime", palette: ["#753915", "#a65c19", "#ba8122", "#dfc142", "#fff55b"]},
+        {name: "Dervish Slime", palette: ["#4e4b6f", "#928cac", "#764c8b", "#41234f", "#85679a"]},
+        {name: "Tangle Slime", palette: ["#186251", "#31a490", "#a2e0c2", "#43581b", "#c7b237"]},
+        {name: "Saber Slime", palette: ["#825d3c", "#b88957", "#ecbf83", "#451b07", "#b89d7d"]},
+    ],
+    harmfulSlimes: [
+        {name: "Rock Slime", palette: ["#002e52", "#034b89", "#1f8fc8", "#324346", "#687374"]},
+        {name: "Rad Slime", palette: ["#032c00", "#116711", "#39c958", "#58fd96", "#8bfc04"]},
+        {name: "Boom Slime", palette: ["#6f0700", "#d41700", "#f35d00", "#f85c01", "#fafc03"]},
+        {name: "Crystal Slime", palette: ["#3f208c", "#6a40b2", "#59a7ee", "#fffe2c", "#f33d15"]},
+        {name: "Fire Slime", palette: ["#6d140a", "#cc220e", "#f63920", "#feaf17", "#ffcd15"]},
+        {name: "Mosaic Slime", palette: ["#fbb561", "#61d388", "#27577f", "#2537d5", "#5036bf"]},
+    ],
+    specialSlimes: [
+        {name: "Quicksilver Slime", palette: ["#2c3a43", "#3e4851", "#758392", "#b3c8d3"]},
+        {name: "Glitch Slime", palette: ["#e6479c", "#42f5b7", "#45f2e6"]},
+        {name: "Gold Slime", palette: ["#ad5718", "#e68623", "#f9e871"]},
+        {name: "Lucky Slime", palette: ["#b6beb6", "#e0dbcd", "#fff7e9", "#f08a24", "#fddb6c"]},
+        {name: "Twinkle Slime", palette: ["#93dfc3", "#c6fde5", "#fd91a8", "#fad1a7", "#f4ffc9"]},
+    ],
+    hostileSlimes: [
+        {name: "Feral Slime", palette: ["#841123", "#63050c", "#fc1d47"]},
+        {name: "Tarr Slime", palette: ["#312421", "#7a6256", "#8e208a", "#30238e", "#2c9377", "#fafcfa"]},
+    ]
+}
 
-const harmfulSlimes = [
-    {name: "Rock Slime", palette: ["#002e52", "#034b89", "#1f8fc8", "#324346", "#687374"]},
-    {name: "Rad Slime", palette: ["#032c00", "#116711", "#39c958", "#58fd96", "#8bfc04"]},
-    {name: "Boom Slime", palette: ["#6f0700", "#d41700", "#f35d00", "#f85c01", "#fafc03"]},
-    {name: "Crystal Slime", palette: ["#3f208c", "#6a40b2", "#59a7ee", "#fffe2c", "#f33d15"]},
-    {name: "Fire Slime", palette: ["#6d140a", "#cc220e", "#f63920", "#feaf17", "#ffcd15"]},
-    {name: "Mosaic Slime", palette: ["#fbb561", "#61d388", "#27577f", "#2537d5", "#5036bf"]},
-];
-
-const specialSlimes = [
-    {name: "Quicksilver Slime", palette: ["#2c3a43", "#3e4851", "#758392", "#b3c8d3"]},
-    {name: "Glitch Slime", palette: ["#e6479c", "#42f5b7", "#45f2e6"]},
-    {name: "Gold Slime", palette: ["#ad5718", "#e68623", "#f9e871"]},
-    {name: "Lucky Slime", palette: ["#b6beb6", "#e0dbcd", "#fff7e9", "#f08a24", "#fddb6c"]},
-    {name: "Twinkle Slime", palette: ["#93dfc3", "#c6fde5", "#fd91a8", "#fad1a7", "#f4ffc9"]},
-];
-
-const hostileSlimes = [
-    {name: "Feral Slime", palette: ["#841123", "#63050c", "#fc1d47"]},
-    {name: "Tarr Slime", palette: ["#312421", "#7a6256", "#8e208a", "#30238e", "#2c9377", "#fafcfa"]},
-];
-
-export { docileSlimes, harmfulSlimes, specialSlimes, hostileSlimes };
+export default slimeIndex;
