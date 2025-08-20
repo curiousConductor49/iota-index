98af57e5954c02c2f05ebaff5ee02792e66bd741
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-20 19:02:34 -0400

    Update card to use props to access slime category

--- src/components/Slimecard.jsx
+++ src/components/Slimecard.jsx
@@ -1,11 +1,10 @@
-import { docileSlimes, harmfulSlimes, specialSlimes, hostileSlimes } from '../lib/slimedata.js'
+import slimeIndex from '../lib/slimedata.js'
 
 function Slimecard(props) {
-    
     return (
         <div>
             <p>Name: {props.name}</p>
-            <div className="flex flex-row">Palette: {docileSlimes[props.slimeNum].palette.map((colour, index) => {
+            <div className="flex flex-row">Palette: {slimeIndex[props.category][props.slimeNum].palette.map((colour, index) => {
                 return <div className="w-[50px] h-[150px]" style={{backgroundColor: colour }} key={index}></div>
             })}</div>
             <img src={props.src} className="h-[150px] w-[150px]"/>
