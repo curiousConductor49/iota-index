948aae01eb66fc93c66c46f0ab6740006bb07eba
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-21 15:05:56 -0400

    Change navbar fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import pink from '../assets/images/docile/Pink_Slime.png'
 
 function Navbar() {
     return (
-        <ul className="border-b-3 border-red-400 font-sans text-2xl flex items-center gap-10 p-5">
-            <li className="mr-auto"><NavLink to="/" className="flex flex-row items-center gap-5"><img src={pink} className="h-[60px] w-[60px]"/><span className="font-sans text-3xl text-border-red-400">Iota Index</span></NavLink></li>
+        <ul className="border-b-3 border-red-400 font-fascinate text-2xl flex items-center gap-10 p-5">
+            <li className="mr-auto"><NavLink to="/" className="flex flex-row items-center gap-5"><img src={pink} className="h-[60px] w-[60px]"/><span className="text-3xl text-border-red-400">Iota Index</span></NavLink></li>
             <li><NavLink to="/" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Home</NavLink></li>
             <li><NavLink to="/categories" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>Categories</NavLink></li>
             <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-red-400" : "text-black" }>About</NavLink></li>
