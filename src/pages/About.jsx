baf57e7fa0cb24b70fcb397491a056003fe6909e
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-21 15:51:25 -0400

    Style fan content notice

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
             <div className="max-w-2xl m-auto flex flex-col gap-10 font-zain">
                 <p className="text-center text-2xl">Slime Rancher was a special part of my childhood growing up. This colour palette index is a little thank you for all the squishy, silly memories I made watching Let's Plays of this game, a world where the scientific colour of cheer was everywhere and the sun always rose on a new adventure in the Far, Far Range.</p>
                 <p className="text-center text-2xl">I hope you enjoy it as much I had fun making it!</p>
-                <p className="text-center text-2xl">Special notice: Iota Index is unofficial Fan Content permitted under <a href="https://www.slimerancher.com/fan-content-policy/">the Fan Content Policy</a>. Not approved/endorsed by Monomi Park. Portions of the materials used are property of Monomi Park, LLC. ©Monomi Park LLC.</p>
+                <p className="text-2xl font-bold rounded-md border-l-5 border-red-600  bg-red-400 text-white p-5">Special notice: Iota Index is unofficial Fan Content permitted under <a href="https://www.slimerancher.com/fan-content-policy/" className="underline">the Fan Content Policy</a>. Not approved/endorsed by Monomi Park. Portions of the materials used are property of Monomi Park, LLC. ©Monomi Park LLC.</p>
             </div>
         </>
     );
