a277954b876c03c14e2fd0d952eb5daaa9f4e11d
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-21 15:24:54 -0400

    Increase font size for links on categories page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -5,7 +5,7 @@ function Categories() {
         <>
             <h1 className="text-center font-fascinate text-5xl m-10">Categories</h1>
             <p className="text-center font-zain text-3xl">Slimes are classed as one of five behaviour types.</p>
-            <ul className="mt-10 text-center font-fascinate text-2xl flex flex-col gap-5">
+            <ul className="mt-10 text-center font-fascinate text-3xl flex flex-col gap-5">
                 <li><Link to="/categories/docile" className="hover:text-red-300 hover:ease-in hover:delay-100">Docile</Link></li>
                 <li><Link to="/categories/harmful" className="hover:text-red-400 hover:ease-in hover:delay-100">Harmful</Link></li>
                 <li><Link to="/categories/hostile" className="hover:text-red-500 hover:ease-in hover:delay-100">Hostile</Link></li>
