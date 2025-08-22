477ab1faddd3bedeac66112f0cdce2ca14b6c4e5
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-22 11:44:55 -0400

    Add comments explaining logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,11 @@ import { NavLink } from 'react-router'
 import pink from '../assets/images/docile/Pink_Slime.png'
 
 function Navbar() {
+    // Returns an unordered list for navigation
+    // Children are list elements which contain navlinks
+    // Each navlink routes to a different page when clicked on
+    // Active state is checked for and text colour styling applied
+    
     return (
         <ul className="border-b-3 border-red-400 font-fascinate text-2xl flex items-center gap-10 p-5">
             <li className="mr-auto"><NavLink to="/" className="flex flex-row items-center gap-5"><img src={pink} className="h-[60px] w-[60px]"/><span className="text-3xl text-border-red-400">Iota Index</span></NavLink></li>
--- src/components/Slimecard.jsx
+++ src/components/Slimecard.jsx
@@ -1,6 +1,12 @@
 import slimeIndex from '../lib/slimedata.js'
 
 function Slimecard(props) {
+    // Returns a div that contains the image, name, and palette of each slime whose data is imported from the slimedata.js file
+    // Is passed props from each slime category page it's called in
+    // props.src provides slime img url
+    // props.category provides key for each nested object containing the slimes per category
+    // props.slimeNum provides the index of each slime object inside the category property (whose value is an array)
+    // key provides a unique identifier for each generated div; this is technically the same value as props.slimeNum, but is saved as a different prop as its purpose is different
     return (
         <div className="border-2 border-red-400 rounded-md w-xl flex flex-col justify-center items-center p-10">
             <div className="flex flex-row gap-5">
@@ -8,10 +14,19 @@ function Slimecard(props) {
                 <div className="self-center flex flex-row items-center gap-2 font-zain">
                     <div className="bg-red-400 text-5xl w-[3px] h-[60px]"></div>
                     <p className="font-semibold text-3xl">Name:</p>
+                    {/* 
+                    Drill down through the slime index object to the slime category, then to the specific slime, then its palette array's first element
+                    Props then passes the actual slime name, which is styled in the colour of that array element
+                    */}
                     <p className="text-3xl" style={{color: slimeIndex[props.category][props.slimeNum].palette[0]}}>{props.name}</p>
                 </div>
             </div>
             <p className="font-semibold font-zain text-2xl">Palette: </p>
+            {/* 
+            Props provides the category key to the slime index object and the specific slime index
+            The palette property of the accessed slime object is walked over to return a div for each palette colour
+            Vanilla CSS used for styling as Tailwind does not permit dynamic classes
+            */}
             <div className="flex flex-row flex-wrap">{slimeIndex[props.category][props.slimeNum].palette.map((colour, index) => {
                 return <div style={{backgroundColor: colour, borderRadius: 5, margin: 5, padding: 10, flexGrow: 1, color: "white", fontFamily: "zain", fontSize: 20}} key={index}>{colour}</div>
             })}</div>
