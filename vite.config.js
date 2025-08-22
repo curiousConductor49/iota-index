a08f978781c2fee2deaea823686fd7a372282dfb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-22 15:19:19 -0400

    Update deploy script and outdir

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
-  base: "/iota-index/",
+  base: "/iota-index",
 })
