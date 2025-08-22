b8588ee0d9e1e85191c43fd86c16e0b4108e8970
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-22 12:02:02 -0400

    Add comments explaining react router

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,21 @@ import PageNotFound from './pages/PageNotFound.jsx'
 function App() {
   return (
     <>
+      {/* Call navbar component so it's present across all pages */}
       <Navbar/>
+      {/* Wrap route elements within the routes parent, which controls which page is rendered based on whether the url is matched */}
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/categories">
+            {/* Categories page has child routes whose urls are built based on their parent */}
             <Route index element={<Categories/>}/>
             <Route path="docile" element={<Docile/>}/>
             <Route path="harmful" element={<Harmful/>}/>
             <Route path="hostile" element={<Hostile/>}/>
             <Route path="special" element={<Special/>}/>
         </Route>
+        {/* The catch-all path redirects user when the url matches anything that hasn't been defined as a route above */}
         <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </>
